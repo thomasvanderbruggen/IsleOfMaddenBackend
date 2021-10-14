bc1d30fd28b5c44ea7effada579b221dee1d5518
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2021-10-14 13:42:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/:platform/:leagueId/standings', (req, res) => {
             console.log(key);
         })
         for (let i = 0; i < teamsWithInfo.length; i++){ 
-            console.log(`${teams[i]['teamId']} ${teamsWithInfo[i]['teamId']}`); 
+            console.log(teams[i]['teamId'] == teamsWithInfo[i]['teamId']); 
         }
         teamsWithInfo = []; 
         res.sendStatus(200); 
