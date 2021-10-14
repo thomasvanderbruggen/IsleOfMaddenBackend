b53521e6adee2b6129cb804c9a8f9b0b5b3a8ba1
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2021-10-14 12:03:41 -0500

    Test project

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express'); 
+
+const app = express(); 
+
+app.set('port', (process.env.PORT || 3001)); 
+
+app.get('*', (req, res) => { 
+    res.send('Testing'); 
+});
+
+app.post('/:platform/:leagueId/leagueTeams', (req, res) => { 
+    let body = ''; 
+    req.on('data', chunk => { 
+        body += chunk.toString(); 
+    }); 
+    req.on('end'), () => { 
+        console.log('----Teams----'); 
+        console.log(JSON.parse(body)); 
+        res.sendStatus(200); 
+    }
+});
+
+app.post('/:platform/:leagueId/standings', (req, res) => { 
+    let body = ''; 
+
+    req.on('data', chunk =>{ 
+        body += chunk.toString(); 
+    }); 
+
+    req.on('end', () => { 
+        console.log('----Standings----'); 
+        console.log(JSON.parse(body)); 
+        res.sendStatus(200); 
+    });
+});
+
+
+app.post('/:platform/:leagueId/week/:weekType/:weekNumber/:dataType', (req, res) => { 
+    let body = ''; 
+
+    req.on('data', chunk => { 
+        body += chunk.toString(); 
+    }); 
+
+    req.on('end', () => {
+        const {params: {dataType},} = req; 
+        console.log(`----${dataType}----`); 
+        console.log(JSON.parse(body));
+        res.sendStatus(200);
+    })
+});
+
+app.post('/:platform/:leagueId/freeagents/roster', (req, res) => { 
+    let body =''; 
+    req.on('data', chunk => { 
+        body += chunk.toString(); 
+    });
+    req.on('end', () => { 
+        console.log('----Free Agents----'); 
+        console.log(JSON.parse(body)); 
+        res.sendStatus(200); 
+    })
+});
+
+app.post('/:username/:platform/:leagueId/team/:teamId/roster', (req, res) => { 
+    let body = '';
+    req.on('data', (req, res) => { 
+        body += chunk.toString();
+    }); 
+    req.end('end', () => { 
+        console.log('---Team Rosters----'); 
+        console.log(JSON.parse(body)); 
+        res.sendStatus(200);
+    });
+});
+
+app.listen(app.get('port'), ()=>{ 
+    console.log('Running on part', app.get('port'));
+});
