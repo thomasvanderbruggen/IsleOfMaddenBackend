787cd0bc6cb4f419f054e21799b2f01366583201
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2021-10-15 11:49:58 -0500

    testing sql

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
-
+const mysql = require('mysql');
+import { dbcredentials } from './secrets/dbcredentials';
 const app = express(); 
 let teamInfoKeys = []; 
 let teamStandingsKeys = [];
@@ -36,8 +37,7 @@ app.post('/:platform/:leagueId/standings', (req, res) => {
     req.on('end', () => { 
         console.log('----Standings----');
         const teams = JSON.parse(body)['teamStandingInfoList'];
-        let team = teams[0]; 
-        Object.keys(team).forEach(key => { 
+        Object.keys(teams[0]).forEach(key => { 
             teamStandingsKeys.push(key);
             console.log(key);
         })
@@ -48,6 +48,29 @@ app.post('/:platform/:leagueId/standings', (req, res) => {
         Object.keys(teams[0]).forEach(key => { 
             console.log(`${key} ${teams[0][key]} ${typeof teams[0][key]}`);
         })
+        let sql = `INSERT INTO teams (awayLosses, awayTies, calendarYear, conferenceId, confLosses, conferenceName,
+             confTies, confWins, capRoom, capAvailable, capSpent, defPassYds, defPassYdsRank,
+             defRushYds, defRushYdsRank, defTotalYds, defTotalYdsRank, divisionId, divLosses, divisionName, divTies,
+             divWins, homeLosses, homeTies, homeWins, netPts, offPassYds, offPassYdsRank, offRushYds, offRushYdsRank,
+             offTotalYds, offTotalYdsRank, ptsAgainstRank, ptsForRank, playoffStatus, prevRank, teamRank, seed, seasonIndex,
+             stageIndex, totalLosses, totalTies, totalWins, teamId, teamName, teamOvr, tODiff, weekIndex, winLossStreak, winPct,
+             abbrName, cityName, defScheme, injuryCount, logoId, nickName, offScheme, ovrRating, primaryColor, secondaryColor, userName
+             VALUES (${team.awayLosses}, ${team.awayTies}, ${team.calendarYear}, ${team.conferenceId}, ${team.confLosses}, ${team.conferenceName},
+                ${team.confTies}, ${team.confWins}, ${team.capRoom}, ${team.capAvailable}, ${team.capSpent}, ${team.defPassYds}, ${team.defPassYdsRank},
+                ${team.defRushYds}, ${team.defRushYdsRank}, ${team.defTotalYds}, ${team.defTotalYdsRank}, ${team.divisionId}, ${team.divLosses},
+                ${team.divisionName}, ${team.divTies}, ${team.divWins}, ${team.homeLosses}, ${team.homeTies}, ${team.homeWins}, ${team.netPts}, ${team.offPassYds},
+                ${team.offPassYdsRank}, ${team.offRushYds}, ${team.offRushYdsRank}, ${team.offTotalYds}, ${team.offTotalYdsRank}, ${team.ptsAgainstRank}, ${team.ptsAgainstRank},
+                ${team.ptsForRank}, ${team.playoffStatus}, ${team.prevRank}, ${team.rank}, ${team.seed}, ${team.seasonIndex}, ${team.stageIndex}, ${team.totalLosses},
+                ${team.totalTies}, ${team.totalWins}, ${team.teamId}, ${team.teamName}, ${team.teamOvr}, ${team.tODiff}, ${team.weekIndex}, ${team.winLossStreak},
+                ${team.winPct}, ${team.abbrName}, ${team.cityName}, ${team.defScheme}, ${team.injuryCount}, ${team.logoId}, ${team.nickName}, ${team.offScheme},
+                ${team.ovrRating}, ${team.primaryColor}, ${team.secondaryColor}, ${team.userName}) ON DUPLICATE KEY UPDATE capRoom=VALUES(capRoom), capSpent=VALUES(capSpent),
+                capAvailable=VALUES(capAvailable), defPassYds=VALUES(defPassYds), defPassYdsRank=VALUES(defPassYdsRank), defRushYds=VALUES(defRushYds), defRushYdsRank=VALUESS(defRushYdsRank), 
+                defTotalYds=VALUES(defTotalYds), defTotalYdsRank=VALUES(defTotalYdsRank), divLosses=VALUES(divLosses), divTies=VALUES(divTies), divWins=VALUES(divWins), homeLosses=VALUES(homeLosses),
+                homeTies=VALUES(homeTies), homeWins=VALUES(homeWins), netPts=VALUES(netPts), offPassYds=VALUES(offPassYds), offPassYdsRank=VALUES(offPassYdsRank), offRushYds=VALUES(offRushYds),
+                offRushYdsRank=VALUES(offRushYdsRank), offTotalYds=VALUES(offTotalYds), offTotalYdsRank=VALUES(offTotatlYdsRank), ptsAgainstRank=VALUES(ptsAgainstRank), ptsForRank=VALUES(ptsForRank),
+                playoffStats=VALUES(playoffStatus), prevRank=VALUES(prevRank), teamRank=VALUES(teamRank), seed=VALUES(seed), seasonIndex=VALUES(seasonIndex), stageIndex=VALUES(stageIndex),
+                totalLosses=VALUES(totalLosses), totalTies=VALUES(totalTies), totalWins=VALUES(totalWins), teamOvr=VALUES(teamOvr), tODiff=VALUES(tODiff), weekIndex=VALUeS(weekIndex),
+                winLossStreak=VALUES(winLossStreak), winPct=VALUES(winPct), defScheme=VALUES(defScheme), injuryCount=VALUES(injuryCount), offSCheme=VALUES(offScheme), ovrRating=VALUES(ovrRating), userName=VALUES(userName)`;
         teamsWithInfo = []; 
         res.sendStatus(200); 
     });
