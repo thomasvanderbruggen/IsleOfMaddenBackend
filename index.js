fb75b161cf7837580cc434c05a29ddac1e621b1b
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2021-10-21 14:34:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express(); 
 const SQL = require('sql-template-strings');
+const cors = require('cors');
 let teamInfoKeys = []; 
 let teamStandingsKeys = [];
 let teamsWithInfo = []; 
