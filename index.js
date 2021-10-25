59bb6faf5888252bab7b627cbf92c4ad4ad790e6
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2021-10-25 16:00:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function calculatePasserRating (stats) {
     let b = (stats.passYdsPerAtt - 3) * .25; 
     let c = (stats.passTDs / stats.passAttempts) * 100 * .2;
     if (c > 2.375) c = 2.375;  
-    let d = 2.375 - (stats.ints / stats.passAttempts) * .25;
+    let d = 2.375 - (stats.ints / stats.passAttempts * 100) * .25;
     return (a + b + c + d) / 6 * 100;  
 }
 
