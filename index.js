bf86e3a0cd99bf2eb79f47bba29195f529792937
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2021-10-25 15:49:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get('/api/roster/:teamId', (req, res) => {
 
 function calculatePasserRating (stats) { 
     let a = (stats.passCompPct - 30) * .05; 
-    let b = (stats.passYdsperAtt - 3) * .25; 
+    let b = (stats.passYdsPerAtt - 3) * .25; 
     let c = (stats.passTDs / stats.passAttempts) * .2; 
     let d = 2.375 - (stats.ints / stats.passAttempts) * .25;
     return (a + b + c + d) / 6 * 100;  
