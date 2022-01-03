86b1a4a95c8c50797b05f9f3932768297f1efc1f
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2022-01-03 12:58:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -507,10 +507,6 @@ app.get('/api/seasonstats/:year/:position/:playerId', (req, res) => {
             if (err) res.sendStatus(500);
             let weeklyStats = []; 
             let response = {}; 
-            response['sql'] = sql; 
-            response['sqlResponse'] = sqlRes; 
-            response['position'] = position; 
-            response['playerId'] = playerId; 
             for (const week of sqlRes) { 
                 seasonStats.name = week.fullName;
                 seasonStats.rushAttempts += week.rushAtt; 
