1c8e6a0cd1e84cdd4d1aedcad12fddd344fbc3e1
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2022-01-11 04:07:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -996,6 +996,7 @@ app.get('/api/standings', (req, res) => {
             for (const game of scheduleRes) { 
                 if (results[game.homeTeamId] === undefined){
                     results[game.homeTeamId] = {}; 
+                    console.log(results[game.homeTeamId]);
                     results[game.homeTeamId]['ptsFor'] = game.homeScore;
                     results[game.homeTeamId]['ptsAgainst'] = game.awayScore;
                 }else { 
@@ -1005,6 +1006,7 @@ app.get('/api/standings', (req, res) => {
 
                 if (results[game.awayTeamId] === undefined){
                     results[game.awayTeamId] = {};
+                    console.log(results[game.awayTeamId]);
                     results[game.awayTeamId]['ptsFor'] = game.awayScore;
                     results[game.awayTeamId]['ptsAgainst'] = game.homeScore;
                 }else{
