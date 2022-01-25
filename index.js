cee4cc3ce4dfda76fe2325c1f8c5cbd3fa6f99ce
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2022-01-24 23:17:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ const pool = mysql.createPool({
 
 app.set('port', (process.env.PORT || 3001)); 
 
-app.use(cors({origin: true}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/', (req, res) => { 
     res.send('Backend for isleofmadden.com'); 
