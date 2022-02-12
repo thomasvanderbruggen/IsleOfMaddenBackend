bcf4ce21ad31e5ba1252166fabb832587664d3c5
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2022-02-12 04:21:25 -0600

    Update postFunctions.js

--- functions/postFunctions.js
+++ functions/postFunctions.js
@@ -60,7 +60,7 @@ function generatePlayerIdWithFirstName(firstName, lastName, rosterId){
 
 function generatePlayerIdWithFullName(fullName, rosterId) {
     let output = `${fullName.charCodeAt(0)}`;
-    const lastName = fullName.slice(3); 
+    const lastName = fullName.slice(2); 
     for (let i = 0; i < lastName.length; i++){
         output += `${lastName.charCodeAt(i)}`;
     }
