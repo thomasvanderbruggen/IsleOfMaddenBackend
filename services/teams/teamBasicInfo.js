2f1e2ef2828eaceed03cf1b848039fc50f7f40dd
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2022-04-23 17:54:17 -0500

    Update teamBasicInfo.js

--- services/teams/teamBasicInfo.js
+++ services/teams/teamBasicInfo.js
@@ -6,14 +6,14 @@ import { dbConfig } from "../../utils";
 //For each team in the json object, insert/update the database
 export const teamBasicInfo = async (teams) => {
     const pool = mysql.createPool(dbConfig);
-    const promisePool = pool.promise();  
+    const promisePool = pool.promise();
     for (const team of teams) { 
         let success = await teamBasicInfoQuery(team, pool);
         if (!success){
             return false;
         } 
     }
-    pool.end();
+    
     return true;
 }
 
