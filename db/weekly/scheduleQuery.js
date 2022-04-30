7501f2b8228104646df5267b07e3cb9419d485f6
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2022-04-30 15:48:43 -0500

    Update scheduleQuery.js

--- db/weekly/scheduleQuery.js
+++ db/weekly/scheduleQuery.js
@@ -8,7 +8,7 @@ export const scheduleQuery = async (game) => {
         let [rows,fields] = await con.query(
         `INSERT INTO schedules (awayScore, awayTeamId, isGameOfTheWeek, homeScore, homeTeamId, scheduleId, seasonIndex, stageIndex, weekStatus, weekIndex) 
         VALUES (?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE awayScore=VALUES(awayScore), awayTeamId=VALUES(awayTeamId), isGameOfTheWeek=VALUES(isGameOfTheWeek), homeScore=VALUES(homeScore), homeTeamId=VALUES(homeTeamId), seasonIndex=VALUES(seasonIndex), weekStatus=VALUES(weekStatus), weekIndex=VALUES(weekIndex)
-        `, [game.awayScore, game.awayTeamId, game.isGameOfTheWeek, game.homeScore, game.homeTeamId, game.scheduleId, game.seasonIndex, game.stageIndex, game.weekStatus, game.weekIndex]); 
+        `, [game.awayScore, game.awayTeamId, game.isGameOfTheWeek, game.homeScore, game.homeTeamId, game.scheduleId, game.seasonIndex, game.stageIndex, game.status, game.weekIndex]); 
         
         con.end();
         return true;
