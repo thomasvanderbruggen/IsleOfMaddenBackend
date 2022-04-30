6311c1d168dacf5a2874281028fa9d6ae45f1f67
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2022-04-30 15:28:25 -0500

    Add the CORS middleware

--- server.js
+++ server.js
@@ -3,23 +3,19 @@ import currentWeek from './db/weekly/currentWeek.js';
 import routes from './routes/index.js'; 
 import { generateTeamIdConversions, dbConfig } from './utils/index.js';
 import mysql from 'mysql2/promise'; 
-
+import cors from 'cors'; 
 
 const app = express(); 
 app.set('port', process.env.PORT || 3001);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors())
 
 app.use('/', routes); 
 
 
 let [seasonIndex, weekIndex] = await currentWeek(); 
 
-app.set('currentSeason', 2); 
+app.set('currentSeason', 3); 
 app.set('currentWeek', weekIndex); 
 const [teamIdToName, teamNameToId] = await generateTeamIdConversions();
 app.set('teamIdToName', teamIdToName);
