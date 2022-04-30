7c55db264b5fb652b23fa19fb9779b17f304aa27
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2022-04-30 14:52:21 -0500

    Update defenseQuery.js

--- db/weekly/defenseQuery.js
+++ db/weekly/defenseQuery.js
@@ -12,11 +12,13 @@ export const defenseQuery = async (stat) => {
         let [rows, fields] = await con.query(
             `INSERT INTO defensive_stats (defCatchAllowed, defDeflections, defForcedFum, defFumRec, defInts, defIntReturnYds, defPts, defSacks, defSafeties, defTDs,
                  defTotalTackles, fullName, rosterId, playerId, scheduleId, seasonIndex, statId, stageIndex, teamId, weekIndex)
-            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) 
             ON DUPLICATE KEY UPDATE defCatchAllowed=VALUES(defCatchAllowed), defDeflections=VALUES(defDeflections), defForcedFum=VALUES(defForcedFum), 
             defFumRec=VALUES(defFumRec), defInts=VALUES(defInts), defIntReturnYds=VALUES(defIntReturnYds), defPts=VALUES(defPts), defSacks=VALUES(defSacks), 
             defSafeties=VALUES(defSafeties), defTDs=VALUES(defTDs), defTotalTackles=VALUES(defTotalTackles), scheduleId=VALUES(scheduleId), seasonIndex=VALUES(seasonIndex), 
-            stageIndex=VALUES(stageIndex), teamId=VALUES(teamId), weekIndex=VALUES(weekIndex)`)
+            stageIndex=VALUES(stageIndex), teamId=VALUES(teamId), weekIndex=VALUES(weekIndex)`,
+             [stat.defCatchAllowed, stat.defDeflections, stat.defForcedFum, stat.defFumRec, stat.defInts, stat.defIntReturnYds, stat.defPts, stat.defSacks, stat.defSafeties, stat.defTDs,
+            stat.defTotalTackles, stat.fullName, stat.rosterId, stat.playerId, stat.scheduleId, stat.seasonIndex, stat.statId, stat.stageIndex, stat.teamId, stat.weekIndex])
         
         con.end();
         return true;
