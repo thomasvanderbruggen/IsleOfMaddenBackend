657996e5834532ce7923b9acef29a4dca5e1c816
thomasvanderbruggen <66325982+thomasvanderbruggen@users.noreply.github.com>
2022-05-24 13:02:18 -0500

    Update schedule.js

--- services/weekly/schedule.js
+++ services/weekly/schedule.js
@@ -6,6 +6,7 @@ import { adjustId } from "../../utils";
     Adjusts the weekIndex, and scheduleId and runs the scheduleQuery for each game provided by the API
 */ 
 export const schedule = async (games, weekType) => {
+    console.log(games[0]);
     for (let game of games) { 
         game.weekIndex++; 
         if (game.weekType === 'pre'){
